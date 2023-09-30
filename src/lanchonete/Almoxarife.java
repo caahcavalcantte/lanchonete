6c85a3bb3874d7659448d8e9fa57c7efb4e5257c
caahcavalcantte <cacavalcantte@gmail.com>
2023-09-30 10:35:35 -0300

    criando package e sub-packege

--- src/lanchonete/Almoxarife.java
+++ src/lanchonete/Almoxarife.java
@@ -1,5 +1,13 @@
 package lanchonete;
 
 public class Almoxarife {
-  
+  public void controlarEntrada(){
+    System.out.println("Controlando entrada dos itens");
+  }
+  public void controlarSaida(){
+    System.out.println("Controlando saida dos itens");
+  }
+  public void entregarIngredientes(){
+    System.out.println("Entregando ingredientes");
+  }
 }
